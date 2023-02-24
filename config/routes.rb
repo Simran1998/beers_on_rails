307f2cb806fc810159a286b72c505cb7e41277dd
Simran Kaur <simran143663@gmail.com>
2023-02-24 12:06:16 -0600

    Seeded Beers with fake data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :beers
+  #get 'companies/index'
+  #get 'companies/show'
+
+  get "/companies", to: "companies#index", as: "companies" #companies_path
+  get "/companies/:id", to: "companies#show", as: "company", constraints: {id: /\d+/} #company_path
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
